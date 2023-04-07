8212015279eb47c4cefe08dc910d666d69fbce1f
Ryan McPeck <mcoso.dev@gmail.com>
2023-04-07 09:04:08 -0400

    fix: scrollable revoke dialog on mobile

--- apps/erc20-delegatable-app/components/cards-sent.tsx
+++ apps/erc20-delegatable-app/components/cards-sent.tsx
@@ -48,9 +48,9 @@ export const CardsSent = ({ className }: CardsSentProps) => {
                     <DialogTrigger>
                       <button className="tag tag-light">Revoke</button>
                     </DialogTrigger>
-                    <DialogContentXL className="lg:p-10">
-                      <div className="grid grid-cols-12 lg:gap-x-10">
-                        <div className="col-span-12 lg:col-span-5">
+                    <DialogContentXL className="overflow-y-auto h-screen sm:max-h-[550px] md:p-10">
+                      <div className="grid grid-cols-12 md:gap-x-10">
+                        <div className="mb-2 col-span-12 md:col-span-5">
                           <h3 className="text-4xl font-normal">How It Works</h3>
                           <hr className="my-4" />
                           <div className="content text-xs">
@@ -66,7 +66,7 @@ export const CardsSent = ({ className }: CardsSentProps) => {
                             to learn more.
                           </div>
                         </div>
-                        <div className="col-span-12 lg:col-span-7">
+                        <div className="col-span-12 md:col-span-7">
                           <CardRender to={received.to} amount={received.amount} decimals={received.decimals} />
                           <div className="my-4 flex flex-col gap-2">
                             <div className="flex items-center justify-between">
