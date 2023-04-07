4c7030e38108c6d9494a8eef9c6ee7f66b40e6ba
Ryan McPeck <mcoso.dev@gmail.com>
2023-04-07 09:01:54 -0400

    fix: scrollable dialog on mobile

--- apps/erc20-delegatable-app/components/cards-received.tsx
+++ apps/erc20-delegatable-app/components/cards-received.tsx
@@ -50,9 +50,9 @@ export const CardsReceived = ({ className }: CardsReceivedProps) => {
                     <DialogTrigger>
                       <button className="tag tag-light">Claim</button>
                     </DialogTrigger>
-                    <DialogContentXL className="lg:p-10">
-                      <div className="grid grid-cols-12 lg:gap-x-10">
-                        <div className="col-span-12 lg:col-span-5">
+                    <DialogContentXL className="overflow-y-auto h-screen sm:max-h-[550px] md:p-10">
+                      <div className="grid grid-cols-12 md:gap-x-10">
+                        <div className="mb-2 col-span-12 md:col-span-5">
                           <h3 className="text-4xl font-normal">How It Works</h3>
                           <hr className="my-4" />
                           <div className="content text-xs">
@@ -69,7 +69,7 @@ export const CardsReceived = ({ className }: CardsReceivedProps) => {
                             to learn more.
                           </div>
                         </div>
-                        <div className="col-span-12 lg:col-span-7">
+                        <div className="col-span-12 md:col-span-7">
                           <CardRender to={card.to} amount={card.amount} decimals={card.decimals} />
                           <div className="my-4 flex flex-col gap-2">
                             <div className="flex items-center justify-between">
